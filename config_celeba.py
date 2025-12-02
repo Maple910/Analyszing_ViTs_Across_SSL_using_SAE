01f2ff7b6d44cd9003ff1fef755833bbec5548bd
NR-SK-010 <sikasikasi029@gmail.com>
2025-12-02 12:44:24 +0900

    重み，可視化結果保存先変更

--- config_celeba.py
+++ config_celeba.py
@@ -11,7 +11,7 @@ D_SAE = D_MODEL * 32
 L1_COEFF = 1e-6
 
 # --- 訓練設定 ---
-GHOST_GRAD_COEFF = 1e-5 
+GHOST_GRAD_COEFF = 1e-5
 LEARNING_RATE = 1e-4
 BATCH_SIZE = 64
 EPOCHS = 2
@@ -25,19 +25,19 @@ CELEBA_ATTR_PATH = os.path.join(CELEBA_BASE_DIR, "list_attr_celeba.txt")
 
 # --- 分析設定 ---
 LAYER_TO_ANALYZE = 10
-K_TOP_UNITS = 20
+K_TOP_UNITS = 1
 NUM_IMAGES_TO_SAMPLE = 5000 
 NUM_IMAGES_TO_VISUALIZE = 9 # 可視化枚数
 
 # --- 分析ターゲット設定 ---
-TARGET_ATTRIBUTE = "Blond_Hair" # 分析したい属性名を設定 (例: "Smiling", "Male")
+TARGET_ATTRIBUTE = "Smiling" # 分析したい属性名を設定 (例: "Smiling", "Male")
 
 # --- SAE重みのパス ---
-SAE_WEIGHTS_DIR = "./data/sae_weights_celeba_run_4" 
+SAE_WEIGHTS_DIR = "./data/sae_weights_celeba/sae_weights_celeba_run_12" 
 SAE_WEIGHTS_PATH_TEMPLATE = os.path.join(SAE_WEIGHTS_DIR, "sae_layer_{layer_idx}.pth")
 
 # --- 結果の保存先 ---
-ANALYSIS_SAVE_DIR = f"./data/analysis_results_{TARGET_ATTRIBUTE}/sae_weights_celeba_run_4"
+ANALYSIS_SAVE_DIR = f"./data/analysis_celeba/analysis_results_{TARGET_ATTRIBUTE}/sae_weights_celeba_run_12"
 ANALYSIS_PATH = os.path.join("./", ANALYSIS_SAVE_DIR)
 
 # --- Weights & Biases (wandb) 設定 ---
